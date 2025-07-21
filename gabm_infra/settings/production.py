4ae05c528772eddd99682be63b64dd683c2b30e2
Jayden Pung <pungkahheng@gmail.com>
2025-07-21 13:20:56 +0800

    Update production.py

--- gabm_infra/settings/production.py
+++ gabm_infra/settings/production.py
@@ -5,9 +5,9 @@ from .base import *
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 # Security settings for production
-SECURE_SSL_REDIRECT = False  # Set to True when using HTTPS
-SECURE_HSTS_SECONDS = 0      # Enable when using HTTPS
+SECURE_SSL_REDIRECT = True  # Set to True when using HTTPS
+SECURE_HSTS_SECONDS = 1      # Enable when using HTTPS
 SECURE_HSTS_INCLUDE_SUBDOMAINS = False
 SECURE_HSTS_PRELOAD = False
-SESSION_COOKIE_SECURE = False  # Set to True when using HTTPS
-CSRF_COOKIE_SECURE = False     # Set to True when using HTTPS
+SESSION_COOKIE_SECURE = True  # Set to True when using HTTPS
+CSRF_COOKIE_SECURE = True     # Set to True when using HTTPS
